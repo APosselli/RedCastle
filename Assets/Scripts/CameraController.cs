637ee449ba5d3fad0e08926b63017634cc93a810
Anthony Posselli <tonioron01@gmail.com>
2018-02-09 19:28:10 -0700

    Add MoveTo script and PlayerController script. Move all player movement to the player controller and update player movement to limit velocity, include "braking", and include other movement directions. Clean up code, adding tooltips and comments.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    [Header("General Camera Settings")]
+    [Tooltip("The player that the camera follows.")]
     public GameObject player;
+    [Tooltip("Sensitivity on the x-axis.")]
     public float xAxisSens = 1f;
+    [Tooltip("Sensitivity on the y-axis.")]
     public float yAxisSens = 1f;
+    [Tooltip("The default distance the camera is from the player.")]
     public float defaultCamDist = 6f;
 
+    // Vector between the player and the camera.
     private Vector3 relativePosition;
+
+    private Rigidbody playerRb;
 
 	// Use this for initialization
 	void Start () {
+        // Hide the mouse and keep it centered.
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
         relativePosition = new Vector3();
+        playerRb = player.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        // Make the camera position relative to the player the same as it was the last frame.
         transform.position = player.transform.position + relativePosition;
+        // Create a vector for moving the camera based on mouse input and sensitivity.
         Vector3 movement = new Vector3(-(Input.GetAxis("Horizontal") * xAxisSens), -(Input.GetAxis("Vertical") * yAxisSens), 0f);
+        // Translate the camera using the movement vector.
         transform.Translate(movement * Time.deltaTime);
 
+        // Use a raycast to determine if anything is between the player and the camera.
+        // Sets the camera distance to either the default distance or the distance to the closest object between the
+        // player and the camera.
         RaycastHit hit;
         float camDistance;
         if (Physics.Raycast(player.transform.position, transform.position - player.transform.position, out hit, defaultCamDist, ~(1 << 7)))
@@ -37,29 +53,25 @@ public class CameraController : MonoBehaviour {
             camDistance = defaultCamDist;
         }
 
+        // Actual camera distance is changed here.
         transform.position = (player.transform.position + camDistance * (transform.position - player.transform.position).normalized);
+        // Make the camera look at the player.
         transform.LookAt(player.transform);
-        Vector3 rotation = transform.rotation.eulerAngles;
-        rotation.x = 0;
-        rotation.z = 0;
-        Quaternion playerRotation = new Quaternion();
-        playerRotation.eulerAngles = rotation;
-        player.GetComponent<Rigidbody>().rotation = playerRotation;
+
+        // Rotate the player to face the same direction as the camera on the y-axis.
+        Vector3 rotationVector = transform.rotation.eulerAngles;
+        rotationVector.x = 0;
+        rotationVector.z = 0;
+        Quaternion rotationQuaternion = new Quaternion();
+        rotationQuaternion.eulerAngles = rotationVector;
+        playerRb.rotation = rotationQuaternion;
         relativePosition = transform.position - player.transform.position;
 
+        // Gives the player control of the mouse again.
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            player.GetComponent<Rigidbody>().AddForce(Vector3.up * 250f);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            player.GetComponent<Rigidbody>().AddForce(player.GetComponent<Rigidbody>().transform.forward * 20f);
         }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    [Header("Movement Settings")]
+    [Tooltip("The max speed the player can travel.")]
+    public float maxVelocity = 10f;
+    [Tooltip("The force applied to the player when a directional key is pressed. Affects acceleration.")]
+    public float movementForce = 20f;
+    [Tooltip("The upwards force applied to the player character when they jump.")]
+    public float jumpForce = 250f;
+    [Tooltip("The force applied to the player to make them come to a stop more quickly. Affects how quickly the player comes to a stop.")]
+    public float brakeForce = 4f;
+    [Tooltip("How slow the player must be moving before braking stops.")]
+    public float brakingVelocity = 2f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 force = new Vector3();
+
+        // Get input for movement on the x, y plane. Apply it to the force.
+        if (Input.GetKey(KeyCode.W))
+        {
+            force += rb.transform.forward;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            force += -rb.transform.forward;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            force += -rb.transform.right;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            force += rb.transform.right;
+        }
+
+        // Make the player come to a stop more quickly.
+        Vector3 playerVelocity = rb.velocity;
+        playerVelocity.y = 0;
+
+        if(force == Vector3.zero && playerVelocity.magnitude > brakingVelocity)
+        {
+            force = -playerVelocity * brakeForce;
+        }
+
+        // Normalize the force for movement on the x, y plane, so moving diagonally doesn't make the player faster.
+        // Multiply by the movement force we can change how fast the player speeds up.
+        force = force.normalized * movementForce;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            force += Vector3.up * jumpForce;
+        }
+
+        rb.AddForce(force);
+
+        // Limit the player speed.
+        if(rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = rb.velocity.normalized * maxVelocity;
+        }
+    }
+}
