4a34d88b76abf41be9b5fb8bc808170ed3de9ea0
Jdao2 <36524724+Jdao2@users.noreply.github.com>
2018-02-22 19:53:51 -0700

    Added four files, moved Nicks files.
    Just moved the files Nick added within the script folder.
    
    And added four files for the dialogue system.

---
+++ Assets/Scripts/_Dialogue/CDT1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Character. Dialogue. Trigger
+public class MoveScene : MonoBehaviour {
+
+    [SerializeField] private string LoadLevel;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LoadLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/_Dialogue/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Whenever we create a class like this and want it to show up...
+// in the inspector so that we can edit it. We need to mark it...
+// as Serializable. So we add this attribute.
+[System.Serializable]
+
+// Dialogue class used as an object that we can pass into the... 
+// DialogueManager whenever we want to start a new dialogue.
+// Host all information that we need about a single dialogue.s
+public class Dialogue {
+
+    // Name of NPC that we're talking of.
+    public string name;
+    public Image charImage;
+
+    // Minimum amount of lines the text area will use and the maximum
+    [TextArea(3, 10)]
+    // Sentences that we will load into our queue.
+    public string[] sentences;
+
+}
